565003a30540c9df4caaa99d8b9014665af5dfaa
duyenthaind <thaind@stringee.com>
2024-08-02 00:14:35 +0700

    feature - update format data of cell

--- src/main/java/com/vitadairy/libraries/importexport/service/ParseCellDefaultService.java
+++ src/main/java/com/vitadairy/libraries/importexport/service/ParseCellDefaultService.java
@@ -1,14 +1,18 @@
 package com.vitadairy.libraries.importexport.service;
 
 import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.DataFormatter;
 
 /**
  * @author duyenthai
  */
 public class ParseCellDefaultService implements ParseCellService {
+
+    protected final DataFormatter dataFormatter = new DataFormatter();
+
     @Override
     public Object parseCellValue(Cell cell) {
-        return cell.getStringCellValue();
+        return dataFormatter.formatCellValue(cell);
     }
 
 }
